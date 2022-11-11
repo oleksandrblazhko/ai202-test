99939029360a1cedb91587da1e854f4f7ba017c9
oleksandrblazhko2 <79433255+oleksandrblazhko2@users.noreply.github.com>
2022-11-11 05:55:18 +0200

    Update OracleCon.java

--- OracleCon.java
+++ OracleCon.java
@@ -17,7 +17,7 @@ class OracleCon {
             ResultSet rs=stmt.executeQuery("select * from emp");  
 		    // get result
             while(rs.next())  
-                System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));    
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));    
             // close the connection object  
             con.close();  
         }
