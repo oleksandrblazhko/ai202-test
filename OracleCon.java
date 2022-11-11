776f2b6e9fd48a485d3da167292c6f3d521f99e6
oleksandrblazhko2 <79433255+oleksandrblazhko2@users.noreply.github.com>
2022-11-11 21:38:54 +0200

    Update OracleCon.java

--- OracleCon.java
+++ OracleCon.java
@@ -3,27 +3,26 @@ import oracle.jdbc.driver.*;
 
 class OracleCon {
     public static void main(final String[] args) {
-		try{  
+	try {  
             // load the driver class  
             DriverManager.registerDriver (new oracle.jdbc.driver.OracleDriver());
             // create connection object
             Connection con=DriverManager.getConnection(  
-                                  "jdbc:oracle:thin:@91.219.60.189:1521/XEPDB1",
-				   			      args[0], // get login as 1th command line parameter
-							      args[1]); // get password as 2nd command line parameter
+                                              "jdbc:oracle:thin:@91.219.60.189:1521/XEPDB1",
+	                                      args[0], // get login as 1th command line parameter
+				              args[1]); // get password as 2nd command line parameter
             // create the statement object  
             Statement stmt=con.createStatement();   
             // execute query  
             ResultSet rs=stmt.executeQuery("select * from emp");  
 		    // get result
             while(rs.next())  
-                System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));    
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));    
             // close the connection object  
             con.close();  
         }
-		catch(Exception e){ 
-		    System.out.println(e);
-		}  
+	catch(Exception e) {
+	     System.out.println(e);
+	}  
     }    
 }  
-
