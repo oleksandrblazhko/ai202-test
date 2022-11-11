1105666bfff468cfefda6b48b37f19be78496b8b
oleksandrblazhko2 <79433255+oleksandrblazhko2@users.noreply.github.com>
2022-11-11 19:45:17 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
-class HelloWorld {
-    public static void main(final String[] args) {
+class HelloWorld{
+    public static void main(String args[]){
         System.out.println("Hello World!");
     }
 }
